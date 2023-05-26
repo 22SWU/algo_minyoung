002bffe301742bc0ffbd42e8a15443509854e6eb
MinY-Lee <leemy08@naver.com>
2023-05-26 17:38:11 +0900

    연결리스트

---
+++ 연결리스트/1158_요세푸스문제.py
@@ -0,0 +1,10 @@
+N, k = map(int, input().split())
+arr = [i for i in range(1, N+1)]
+answer = []
+num = 0
+for i in range(N):
+    num += k-1
+    if num >= len(arr):
+        num = num%len(arr)
+    answer.append(str(arr.pop(num)))
+print("<", ", ".join(answer),">", sep = "")
---
+++ 연결리스트/5397_키로거.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(input())
+
+for i in range(0,n):
+    password = input()
+    answer = ""
+    left = []
+    right = []
+    for j in password:
+        if j == '-':
+            if left:
+                left.pop()
+        elif j == '<':
+            if left:
+                right.append(left.pop())
+        elif j == '>':
+            if right:
+                left.append(right.pop())
+        else:
+            left.append(j)
+        print(left)
+        print(right)
+    left.extend(reversed(right))
+    print(''.join(left))
+
